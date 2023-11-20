971efa09a25b483deb00646f3829b912db17b32a
ɅLΞX <123724383+ALEXWARELLC@users.noreply.github.com>
2023-11-20 02:22:22 +0000

    Initial Plugin Dump

---
+++ Config.cs
@@ -0,0 +1,9 @@
+using Exiled.API.Interfaces;
+
+namespace AutoKOS;
+
+public class Config : IConfig
+{
+    public bool IsEnabled { get; set; }
+    public bool Debug { get; set; }
+}
---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Exiled.Events.EventArgs.Player;
+
+namespace AutoKOS;
+
+public class Plugin : Plugin<Config>
+{
+    public override string Name => "AutoKOS";
+    public override string Author => "ALEXWARELLC";
+    public override string Prefix => "autokos";
+    public override Version Version => new Version(1, 0, 0);
+
+    public override void OnEnabled()
+    {
+        // Initialize KOS Detection
+        Exiled.Events.Handlers.Player.Died += OnPlayerKilled;
+    }
+
+    public void OnPlayerKilled(DiedEventArgs ev)
+    {
+        if(ev.Player.IsCuffed)
+        {
+            
+        }
+    }
+
+}
